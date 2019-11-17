5248e03dc636614e69abd69507a6ff8499ababc7
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-11-17 15:09:58 +0000

    Some change to folders

---
+++ education_purpose/ex*/pid/ppid.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - PID
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	pid_t my_ppid; /* PARENT PROCESS PID */
+
+	my_ppid = getppid();
+	printf("%u\n", my_ppid);
+	return (0);
+}
--- education_purpose/ex*/simple.h
+++ education_purpose/ex*/simple.h
@@ -3,5 +3,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #define shsign printf("$ ");
 #endif
