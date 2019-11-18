de551f0d8c8797aaaeef55a37a246460055573fa
YassineChayrrou <yassine.chayrrou@gmail.com>
2019-11-18 15:12:02 +0000

    fixed finally

--- education_purpose/exyz/execve.c
+++ education_purpose/exyz/execve.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
-  works just fine only problem it does not run all command for example
-  type vim and it would give you error i think it is related to the second
-  argument of execve
+  works just fine just if you want to run any process you need to type its directory and add options like example in the arguments section very cool hein
  */
 int main(int argc, char *argv[])
 {
-    printf("Before execve\n");
-    if (argc == 2)
+    if (argc > 1)
     {
 	    if (execve(argv[1], argv, NULL) == -1)
 		    puts("try again");
     }
     else
-	    puts("too many arguments");
+	    puts("fix you arguments count");
 }
