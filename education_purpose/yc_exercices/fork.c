48cfff6d8c8f27b502dd59cc7c8dd5fccc89a55d
YassineChayrrou <yassine.chayrrou@gmail.com>
2019-11-20 13:42:29 +0000

    new update

---
+++ education_purpose/yc_exercices/fork.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib>
+#include <unistd.h>
+/**
+  *main - a simple fork program
+  *NOTE: if condition for child identification will be used to prevent child from forking another process
+  */
+int main()
+{
+	pid_t fpid;
+
+
+}
