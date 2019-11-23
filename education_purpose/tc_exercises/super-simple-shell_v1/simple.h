e3a419fc5f8f59a9ddd93bbe15022b7f027985ba
Taieb CHaabini <taiebchaabini@gmail.com>
2019-11-23 20:27:58 +0000

    Changing directory name

---
+++ education_purpose/tc_exercises/super-simple-shell_v1/simple.h
@@ -0,0 +1,12 @@
+#ifndef simple_h
+#define simple_h
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#define shsign printf("#cisfun$ ");
+#define delim " \n"
+int _strlen(char *s);
+#endif
