a5ce6c96c9bb4b0929bf5bfd9e5ab8a023417ab1
YassineChayrrou <yassine.chayrrou@gmail.com>
2019-11-23 02:12:08 +0000

    forked success

--- education_purpose/yc_exercices/fork.c
+++ education_purpose/yc_exercices/fork.c
@@ -1,13 +1,35 @@
 #include <stdio.h>
-#include <stdlib>
+#include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 /**
   *main - a simple fork program
   *NOTE: if condition for child identification will be used to prevent child from forking another process
   */
 int main()
 {
-	pid_t fpid;
+	pid_t chpid;
+	int status, i = 0;
+	char *argv[] = {"/bin/ls", "-l", "/tmp/", NULL};
 
-
+	while (i < 5)
+	{
+		chpid = fork();
+		if (chpid < 0)
+		{
+			perror("error");
+			exit(-1);
+		}
+		if (chpid == 0)
+		{
+			execve(argv[0], argv, NULL);
+		}
+		else
+		{
+			wait(&status);
+		}
+		i++;
+	}
+	return (0);
 }
