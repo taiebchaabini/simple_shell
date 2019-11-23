f5fd844121affe54342f40f427db96713c829469
Taieb CHaabini <taiebchaabini@gmail.com>
2019-11-23 19:56:45 +0000

    Super simple shell v0

--- education_purpose/tc_exercises/super_simple_shell.c
+++ education_purpose/tc_exercises/super_simple_shell.c
@@ -15,15 +15,16 @@ int main(void)
 	shsign
 	while ((read = getline(&line, &len, stdin)) != -1)
 	{
-		children_pid = fork();
 		str = malloc(sizeof(char) * strlen(line));
 		if (str == NULL)
 			exit(-1);
+		*argv = NULL;
 		*argv = malloc(sizeof(char) * strlen(line));
 		if (argv == NULL)
 			exit(-1);
 		str = strcpy(str, line);
 		token = strtok(str, delim);
+		i = 0;
 		while (token != NULL)
 		{
 			argv[i] = malloc(sizeof(char) * strlen(token));
@@ -34,13 +35,16 @@ int main(void)
 			i++;
 		}
 		argv[i] = NULL;
+		children_pid = fork();
 		if (children_pid == 0)
 		{
 			execve(argv[0], argv, NULL);
 		}
-			shsign
-			wait(0);
+		shsign
+		wait(0);
 	}
 	free(line);
+	free(str);
+	free(token);
 	exit(EXIT_SUCCESS);
 }
