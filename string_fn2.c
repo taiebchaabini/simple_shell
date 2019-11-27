76cb480944a02b91f7c39e374b7fc03ef839f84c
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-11-27 22:39:15 +0000

    Updates

--- string_fn2.c
+++ string_fn2.c
@@ -50,7 +50,6 @@ void *_calloc(unsigned int nmemb, unsigned int size)
  * free_grid - function that frees a 2 dimensional grid previously created by
  * your alloc_grid function.
  * @grid: the address of the two dimensional grid
- * @height: height of the grid
 **/
 void free_grid(char **grid)
 {
