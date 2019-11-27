1798243864e16221318e2fa0ce7718233c6bb451
Taieb Chaâbini <taiebchaabini@gmail.com>
2019-11-27 16:26:51 +0000

    Updates

--- string_fn2.c
+++ string_fn2.c
@@ -26,7 +26,7 @@ char *_strdup(const char *str)
  * _calloc - function that allocates memory for an array, using malloc.
  * @nmemb: number of elements
  * @size: size of type
-i * Return: pointer
+ i * Return: pointer
  **/
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
@@ -47,4 +47,20 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		i++;
 	}
 	return (p);
+}
+/**
+ * free_grid - function that frees a 2 dimensional grid previously created by
+ * your alloc_grid function.
+ * @grid: the address of the two dimensional grid
+ * @height: height of the grid
+**/
+void free_grid(int **grid, int height)
+{
+	int h = 0;
+
+	for (; h < height; h++)
+	{
+		free(grid[h]);
+	}
+	free(grid);
 }
